bbd43ff22c1f45847be2ea75fe3dab2187c599f6
Ahmed Atta <ahmed.x.atta@pwc.com>
2025-06-02 18:22:28 +0300

    c3

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -11,26 +11,3 @@ public class DemoApplication {
     }
 }
 
-// src/main/java/com/example/demo/EmailController.java
-package com.example.demo;
-
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
-@RestController
-@RequestMapping("/api")
-public class EmailController {
-    private List<String> emails = new ArrayList<>();
-
-    @PostMapping("/email")
-    public void addEmail(@RequestBody String email) {
-        emails.add(email);
-    }
-
-    @GetMapping("/emails")
-    public List<String> getEmails() {
-        return emails;
-    }
-}
